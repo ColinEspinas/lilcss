e320534dfc728e4df65dad9a92bbaac3e2abc1bb
ColinEspinas <colin.espinas@yahoo.com>
2019-04-05 21:39:23 +0200

    Added lil.css to Git

---
+++ js/script.js
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+var codeExempleGrid1 = document.getElementById('codeExempleGrid1');
+
+var codeExempleGrid1Text = 
+`<div class="lil-row">
+    <div class="lil-col -6-12">
+        <p>I am column!</p>
+    </div>
+    <div class="lil-col -6-12">
+        <p>I another column!</p>
+    </div>
+</div>
+`;
+
+codeExempleGrid1.innerHTML= codeExempleGrid1Text;
+
+
+
+
+
+
+
+
+var codeExempleGrid2 = document.getElementById('codeExempleGrid2');
+
+var codeExempleGrid2Text = 
+`<div class="lil-row -between">
+    <div class="lil-col -half">
+        <section class="lil-section">
+            <div class="lil-section-header">
+                <h2 class="lil-section-title">Section 1</h2>
+                <hr>
+            </div>
+            <div class="lil-section-body">
+                <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Asperiores, illum ut et
+                    accusantium ipsam, suscipit perferendis omnis, distinctio autem commodi aliquam
+                    aut odio voluptates sed qui vitae. Molestias, ab repellat.
+                </p>
+                <button class="lil-btn orange width-100">Click me, I'm a button!</button>
+            </div>
+        </section>
+    </div>
+    <div class="lil-col -half">
+        <section class="lil-section">
+            <div class="lil-section-header">
+                <h2 class="lil-section-title">Section 2</h2>
+                <hr>
+            </div>
+            <div class="lil-section-body">
+                <p class="text-justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Ipsum repellendus
+                    cumque explicabo ducimus voluptatum debitis magnam quod, itaque, nostrum, vel
+                    at reprehenderit odio illum eos mollitia! Ad cupiditate repellendus unde.
+                </p>
+                <button class="lil-btn green width-100">Click me, I'm a button!</button>
+            </div>
+        </section>
+    </div>
+</div>`;
+
+codeExempleGrid2.innerHTML= codeExempleGrid2Text;
