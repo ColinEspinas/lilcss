6d8b2d32390da3c5698f662c39ba7877b58c73ea
ColinEspinas <colin.espinas@yahoo.fr>
2019-04-06 13:50:56 +0200

    Updated grid for better responsive Defined some media-queries for future updates

--- js/script.js
+++ js/script.js
@@ -29,39 +29,14 @@ codeExempleGrid1.innerHTML= codeExempleGrid1Text;
 var codeExempleGrid2 = document.getElementById('codeExempleGrid2');
 
 var codeExempleGrid2Text = 
-`<div class="lil-row -between">
-    <div class="lil-col -half">
-        <section class="lil-section">
-            <div class="lil-section-header">
-                <h2 class="lil-section-title">Section 1</h2>
-                <hr>
-            </div>
-            <div class="lil-section-body">
-                <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Asperiores, illum ut et
-                    accusantium ipsam, suscipit perferendis omnis, distinctio autem commodi aliquam
-                    aut odio voluptates sed qui vitae. Molestias, ab repellat.
-                </p>
-                <button class="lil-btn orange width-100">Click me, I'm a button!</button>
-            </div>
-        </section>
+`<div class="lil-row between">
+    <div class="lil-col 1-12">
+        <p>1-12</p>
     </div>
-    <div class="lil-col -half">
-        <section class="lil-section">
-            <div class="lil-section-header">
-                <h2 class="lil-section-title">Section 2</h2>
-                <hr>
-            </div>
-            <div class="lil-section-body">
-                <p class="text-justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Ipsum repellendus
-                    cumque explicabo ducimus voluptatum debitis magnam quod, itaque, nostrum, vel
-                    at reprehenderit odio illum eos mollitia! Ad cupiditate repellendus unde.
-                </p>
-                <button class="lil-btn green width-100">Click me, I'm a button!</button>
-            </div>
-        </section>
+    <div class="lil-col 11-12">
+        <p>11-12</p>
     </div>
-</div>`;
+</div>
+`;
 
 codeExempleGrid2.innerHTML= codeExempleGrid2Text;
